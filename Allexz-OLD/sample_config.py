3abc40bb1b6e4341c5a1efd7fafec13a847f27a8
OMINDA DILSHAN RESPACT <85678266+omindadelshan@users.noreply.github.com>
2021-09-30 13:03:35 +0530

    Update sample_config.py

--- Allexz-OLD/sample_config.py
+++ Allexz-OLD/sample_config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/AnkiVector/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Allexz-OLD/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
